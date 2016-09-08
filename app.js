9e6029a95d336cfdb9dfb3b3d2ea835ff2de9254
JaviSG91 <javisago91@gmail.com>
2016-09-08 13:36:06 -0700

    get index

--- app.js
+++ app.js
@@ -41,10 +41,8 @@ console.log("Server iniciado");
 
 vision.get('/',function(req,res){
 var pagina='<!doctype html><html><head></head><body>';   
-    pagina+= '<form action=\"Registro\" method=\"post\">';
-    pagina += '<br><a href="index.html">Link</a></br>';
-    //pagina += '<br><input type=\"submit\" value=\"Registrar\"></br>';
-    
+    pagina+= '<form action=\"Resend\" method=\"post\">';
+    pagina += '<br><a href="/public/index.html">Link</a></br>';  
     pagina += '</form>';
     pagina += '</body></html>';
 	res.send(pagina);
